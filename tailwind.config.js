d44cb66bba30cd44b3da410aafc8816ce705207e
Adithya S K <adithyaskolavi@gmail.com>
2023-01-14 14:32:11 +0530

    set up folder with components - Adithya S K

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#484848',
+        secondary: '#707070',
+        background: '#001322',
+        accent_dark: '#0EA5E9',
+        accent_light: '#38BDF9',
+        // for transparent background
+        shade: 'rgba(0, 0, 0, 0.1)',
+        shade2: 'rgba(255, 255, 255, 0.6)',
+        shade3: 'rgba(255, 255, 255, 0.9)',
+      },
+    },
   },
   plugins: [],
-}
+};
