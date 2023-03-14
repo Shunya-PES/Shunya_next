e2e1928e170aeb9af0a368312b6dd5d39894a9eb
Sathvik Murthy <sathvikmurthy31@gmail.com>
2023-03-14 21:31:51 +0530

    added env

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCh6r68XCJ7rQ5GTJn_tjIhRx48rq-OHVU",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: "arithmania-d394d.firebaseapp.com",
     projectId: "arithmania-d394d",
     storageBucket: "arithmania-d394d.appspot.com",
     messagingSenderId: "526647876215",
-    appId: "1:526647876215:web:fba72197d25ca6ef3a0472",
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     measurementId: "G-SQLKBRPBJS"
 }
 
